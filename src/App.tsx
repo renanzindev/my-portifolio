91171cd41392af320bf3e3b6c6f7ba90207a5e4f
AdrianaSaty <adri-saty@hotmail.com>
2025-02-24 23:10:48 -0300

    feat: installs libs and implements components at App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import AboutSection from "./Pages/Home/sections/AboutSection/AboutSection"
+import HeroSection from "./Pages/Home/sections/HeroSection/HeroSection"
+import Navbar from "./components/Navbar/Navbar"
+import ProjectsSection from "./Pages/Home/sections/ProjectsSection/ProjectsSection"
+import Footer from "./components/Footer/Footer"
 
-function App() {
+const App: React.FC = () => {
 
   return (
     <>
-      hey
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <ProjectsSection />
+      <Footer />
     </>
   )
 }
