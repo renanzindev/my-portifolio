e432dee04db62c48b65130421542dcf2148adbd1
AdrianaSaty <adri-saty@hotmail.com>
2025-02-24 23:13:24 -0300

    refactor: deletes unused code at ProjectsSection

--- src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
+++ src/Pages/Home/sections/ProjectsSection/ProjectsSection.tsx
@@ -54,19 +54,6 @@ const ProjectsSection: React.FC = () => {
                 <Box id="projects" pt={5} pb={3}>
                     <Typography variant="h2" textAlign="center" color="primary.contrastText">Projects</Typography>
                 </Box>
-                {/* <Grid container spacing={3}>
-                    <Grid item md={6}>
-                        <ProjectCard
-                            title="Project Craze Maze"
-                            subtitle="Jul 2019 - May 2019"
-                            srcImg="/src/assets/images/project1-craze-maze.gif"
-                            description="Game to escape the maze, but not only that. An algorithm has been created that randomly generates a new maze each time the game is started. In this game, the user can use the keyboard keys to move until they find the flag and win the game"
-                            technologies="Technologies: JavaScript, HTML, CSS, Canvas Graphics"
-                            websiteURL="https://adrianasaty.github.io/ironhack-project1-craze-maze/index.html"
-                            codeURL="https://github.com/AdrianaSaty/ironhack-project1-craze-maze"
-                        />
-                    </Grid>
-                </Grid> */}
                 <Grid container spacing={5} pb={3}>
                     {projects.map((project: ProjectCardProps, index: number) => (
                         <Grid item md={6} key={index}>
